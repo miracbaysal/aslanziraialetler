823dd1d1b83ca337655c71cfed211b8ef324fae2
Miraç Baysal <mirac.bysal@gmail.com>
2025-04-22 17:26:29 +0300

    some css updates

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -36,7 +36,11 @@
 	<link rel="stylesheet" href="assets/css/responsive.css">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
-        
+        <style>
+			 a {
+				text-decoration:none;
+			} 
+		</style>
 </head>
 <body>
 
